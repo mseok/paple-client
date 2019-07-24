93c9ec7ab6f6d133fc3f014daa498ad80f2ce17f
Sunghwan Kim <ssshwan.korea@gmail.com>
2019-07-24 17:37:48 +0900

    add network connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './client/Root';
+import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
+import client from "./apollo"
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const App = (
+    <ApolloProvider client={client}>
+      <Root />
+    </ApolloProvider>
+  );
+  
+
+ReactDOM.render(App, document.getElementById('root'));
 serviceWorker.unregister();
